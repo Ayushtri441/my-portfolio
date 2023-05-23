20c2e74f44a16a18b2d7d9b13b87fba6a088079b
Ayush Tripathi <113000672+Ayushtri441@users.noreply.github.com>
2023-05-23 16:32:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed("#t", {
+      strings: [
+        "Competitive Coder",
+        "Full Stack Developer",
+        "Backend Developer",
+        "Java Developer",
+        "Web Developer",
+        "Web Designer",
+        "Freelancer",
+        "Open Source Contributor"
+      ],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    });
+  });
